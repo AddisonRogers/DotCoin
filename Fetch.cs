5a6359206b81fff1c4defa3850944d9c87d0fafb
AddisonRogers <87341723+AddisonRogers@users.noreply.github.com>
2022-07-02 08:28:43 +0100

    Initial Commit

--- Fetch.cs
+++ Fetch.cs
@@ -1,6 +1,65 @@
 ﻿namespace DotCoinWPF;
 
-public class Fetch
+using System;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+
+public static class Fetch
 {
+    public static async Task<JsonNode?> GetAll()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://api.coincap.io/v2/assets");
+            return JsonNode.Parse(await response.Content.ReadAsStringAsync())?["data"];
+        }
     
+        public static async Task<double[]?> History(string? id, int timeValue, string? interval)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync($"https://api.coincap.io/v2/assets/{id}/history?interval={interval}");
+            var json = JsonNode.Parse(await response.Content.ReadAsStringAsync())?["data"];
+            var count = json.AsArray().Count -1;
+            double[] prices = new double[timeValue];
+            int x = count - timeValue;
+            int counter = -1;
+            for (int i = count; i > x; i--)
+            {
+                counter++;
+                prices[counter] = double.Parse(json[i]?["priceUsd"]?.ToString() ?? string.Empty);
+            }
+
+            return prices; //needs to be relooked and tested
+    
+        }
+
+        public static async Task<JsonNode?> Get(string? symbol)
+        {
+
+            JsonNode? json = await GetAll();
+            if (json != null)
+            {
+                var count = json.AsArray().Count;
+                try
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (json[i]?["symbol"]?.ToString() == symbol)
+                        {
+                            return json[i];
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+
+            }
+
+            return null; //error checking
+        }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-﻿namespace DotCoinWPF;
+﻿using System.Threading.Tasks;
+
+namespace DotCoinWPF;
+using System;
 
 public class Program
 {
-    
+    public static async Task main()
+    {
+        //https://api.coincap.io/v2/assets is the api site for the coin data
+
+//Console.WriteLine(await Fetch.GetAll());
+
+
+        Console.WriteLine("Which coin would you like to know about?");
+        var coinSymbol = "BTC";
+        var coinFlag = false;
+        while (coinFlag == false)
+        {
+            coinSymbol = Console.ReadLine()?.ToUpper().Trim();
+            if ((coinSymbol == null) | (await Fetch.Get(coinSymbol) == null))
+            {
+                Console.WriteLine("Please enter a valid coin");
+
+            }
+            else
+            {
+                coinFlag = true;
+            }
+
+
+        }
+
+        var coin = await Fetch.Get(coinSymbol);
+        double price = double.Parse(coin?["priceUsd"]?.ToString() ?? string.Empty);
+        string name = coin?["id"]?.ToString().Trim().ToLower() ?? string.Empty;
+
+//Console.WriteLine($"The price of {name} is {price}");
+//double[]? prices = (await Fetch.History(name, 5, "h1"));
+//foreach (double i in prices){Console.WriteLine(i);}
+        Console.WriteLine(await Indicator.MovingAverage(name));
+    }
 }
