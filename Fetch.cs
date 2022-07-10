c51c7b5a4e705a309ebcc6dcb7e6c0cc28db9620
AddisonRogers <87341723+AddisonRogers@users.noreply.github.com>
2022-07-10 17:34:22 +0100

    Updated History again woopsies

--- Fetch.cs
+++ Fetch.cs
@@ -19,21 +19,18 @@ public static class Fetch
             using HttpClient client = new HttpClient();
             const int d = 86400000; const int h = 3600000; const int m = 60000;
             long timeperiod = 0;
-            
-            
+
             if (interval?.ToCharArray()[0].ToString() == "m") timeperiod = (DateTimeOffset.Now.ToUnixTimeMilliseconds() - (m * int.Parse(interval?.ToCharArray()[1].ToString()!) * timeValue));
             else if (interval?.ToCharArray()[0].ToString() == "h") timeperiod = (DateTimeOffset.Now.ToUnixTimeMilliseconds() - (h * int.Parse(interval?.ToCharArray()[1].ToString()!) * timeValue));
             else if (interval?.ToCharArray()[0].ToString() == "d") timeperiod = (DateTimeOffset.Now.ToUnixTimeMilliseconds() - ((d * int.Parse(interval?.ToCharArray()[1].ToString()!)) * timeValue));
             else Console.WriteLine("Help");
-            JsonNode? json = null;
-            while (json == null){json = JsonNode.Parse(client.GetStringAsync($"https://api.coincap.io/v2/assets/{id}/history?interval={interval}&start={timeperiod}&end=" + $"{DateTimeOffset.Now.ToUnixTimeMilliseconds()}").Result)?["data"];}
+            JsonNode? json = JsonNode.Parse(client.GetStringAsync($"https://api.coincap.io/v2/assets/{id}/history?interval={interval}&start={timeperiod}&end=" + $"{DateTimeOffset.Now.ToUnixTimeMilliseconds()}").Result)?["data"];
             //JsonNode? json = JsonNode.Parse(client.GetStringAsync($"https://api.coincap.io/v2/assets/{id}/history?interval={interval}&start={timeperiod}&end=" + $"{DateTimeOffset.Now.ToUnixTimeSeconds()}").Result)?["data"];
-            double[] priceList = new double[json.AsArray().Count];
+            double[] priceList = new double[json!.AsArray().Count];
             for (int i = 0; i != json.AsArray().Count; i++)
             {
                 priceList[i] = double.Parse(json[i]?["priceUsd"]?.ToString()!);
             }
-
             return priceList;
         }
         public static JsonNode? Get(string? symbol)
