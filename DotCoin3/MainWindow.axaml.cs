7e1cf52f827508692b536c89176449b6283cc8ae
Addison <addisonrogers@protonmail.com>
2022-10-03 19:16:41 +0100

    Base

---
+++ DotCoin3/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DotCoin3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
