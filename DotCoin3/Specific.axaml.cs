ee0dafbd83400d002a46512fb82d4d41f3b59a66
Addison <addisonrogers@protonmail.com>
2022-10-04 18:03:14 +0100

    School Work

--- DotCoin3/Specific.axaml.cs
+++ DotCoin3/Specific.axaml.cs
@@ -13,7 +13,7 @@ public partial class Specific : UserControl
         SetText();
     }
 
-    public string id = "bitcoin";
+    public string id;
     private void SetText()
     {
         var name = id;
@@ -21,14 +21,14 @@ public partial class Specific : UserControl
         this.Find<TextBlock>("SymbolBox").Text = "Symbol : " + coin?["symbol"]?.ToString();
         this.Find<TextBlock>("NameBox").Text = "Name : " + coin?["name"]?.ToString();
         this.Find<TextBlock>("SupplyBox").Text = "Circulating Supply : " + Math.Round(double.Parse(coin?["supply"]?.ToString() ?? string.Empty), 0);
-        
+
         //This if is done becuause some crypto have unlimited supply
         if (coin?["maxSupply"]?.ToString() != null) this.Find<TextBlock>("MaxSupplyBox").Text = "Max Supply : " + Math.Round(double.Parse(coin?["maxSupply"]?.ToString() ?? string.Empty), 0);
         else this.Find<TextBlock>("MaxSupplyBox").Text = "Max Supply : " + "N/A";
-        MarketCapBox.Text = "Market Cap : " + Math.Round(double.Parse(coin?["marketCapUsd"]?.ToString() ?? string.Empty), 0);
-        PriceBox.Text = "Price : $" + Math.Round(double.Parse(coin?["priceUsd"]?.ToString() ?? string.Empty), 2);
-        Change24HBox.Text = "Volume change in 24h : " + Math.Round(double.Parse(coin?["volumeUsd24Hr"]?.ToString() ?? string.Empty), 0);
-        ChangeP24HBox.Text = "% Change in 24h : " + Math.Round(double.Parse(coin?["changePercent24Hr"]?.ToString() ?? string.Empty), 1) + "%";
+        this.Find<TextBlock>("MarketCapBox").Text = "Market Cap : " + Math.Round(double.Parse(coin?["marketCapUsd"]?.ToString() ?? string.Empty), 0);
+        //this.Find<TextBlock>("PriceBox").Text = "Price : " + Math.Round(double.Parse(coin?["price"]?.ToString() ?? string.Empty), 2);
+        //this.Find<TextBlock>("Change24HBox").Text = "Change 24H : " + Math.Round(double.Parse(coin?["change24h"]?.ToString() ?? string.Empty), 2); 
+        //this.Find<TextBlock>("ChangeP24HBox").Text = "Change % 24H : " + Math.Round(double.Parse(coin?["changeP24h"]?.ToString() ?? string.Empty), 2);
         //MovingAverageBox.Text = "Moving Average : " + Math.Round(double.Parse(Indicator.MovingAverage(name)?.ToString() ?? string.Empty), 0);
     }
     private void InitializeComponent()
