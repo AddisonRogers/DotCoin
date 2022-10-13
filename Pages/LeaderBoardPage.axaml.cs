8c05dfd028302197e07926e8c78eb5bf543f2a88
AddisonRogers <87341723+AddisonRogers@users.noreply.github.com>
2022-10-13 10:46:16 +0100

    New New Structure

---
+++ Pages/LeaderBoardPage.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DotCoin3.Pages;
+
+public partial class LeaderBoardPage : UserControl
+{
+    public LeaderBoardPage()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
