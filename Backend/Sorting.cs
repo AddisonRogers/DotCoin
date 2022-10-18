6fac64a546ca75fbdd03bd2a75e2df3984faa448
AddisonRogers <87341723+AddisonRogers@users.noreply.github.com>
2022-10-18 12:59:54 +0100

    Yes

---
+++ Backend/Sorting.cs
@@ -0,0 +1,63 @@
+﻿namespace DotCoin3;
+
+public class Sorting
+{
+    public object Sort(string type = "Price", object ToSort = null, string SortType = "Linear")
+    {
+        if (ToSort == null)
+        {
+            return null;
+        }
+        else
+        {
+            switch (SortType)
+            {
+                case "Linear":
+                    return LinearSort(ToSort, type);
+                case "Bubble":
+                    return BubbleSort(ToSort, type);
+                case "Merge":
+                    return MergeSort(ToSort, type);
+                case "Quick":
+                    return QuickSort(ToSort, type);
+                case "Heap":
+                    return HeapSort(ToSort, type);
+                case "Insertion":
+                    return InsertionSort(ToSort, type);
+                case "Radix":
+                    return RadixSort(ToSort, type);
+                default:
+                    break;
+            }
+        }
+    }
+    private object LinearSort(object ToSort, string type)
+    {
+        return null;
+    }
+    private object BubbleSort(object ToSort, string type)
+    {
+        return null;
+    }
+    private object MergeSort(object ToSort, string type)
+    {
+        return null;
+    }
+    private object QuickSort(object ToSort, string type)
+    {
+        return null;
+    }
+    private object HeapSort(object ToSort, string type)
+    {
+        return null;
+    }
+    private object InsertionSort(object ToSort, string type)
+    {
+        return null;
+    }
+    private object RadixSort(object ToSort, string type)
+    {
+        return null;
+    }
+    
+}
--- Pages/LeaderBoardPage.axaml.cs
+++ Pages/LeaderBoardPage.axaml.cs
@@ -101,7 +101,7 @@ public partial class LeaderBoardPage : UserControl
         //TODO Make the chart use a time system rather than relying off units so that I can upload a full minute for the past like year
     }*/
 
-    private void CryptoInfoTextBlockSet() => this.Find<TextBlock>("CryptoInfoTextBlock").Text = System.IO.File.ReadAllText("C:\\Users\\Addison\\RiderProjects\\DotCoin\\Pages\\CryptoInfo.txt");
+    private void CryptoInfoTextBlockSet() => this.Find<TextBlock>("CryptoInfoTextBlock").Text = System.IO.File.ReadAllText("C:\\Users\\icare\\Documents\\Coding\\DotCoin\\Pages\\CryptoInfo.txt");
     private void CryptoSet()
     {
         var AllCrypto = Fetch.GetAll();
